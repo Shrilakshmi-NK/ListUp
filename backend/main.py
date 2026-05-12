ae14e443bb640b74a1cfbdaf7339d6e851910783
Shrilakshmi-NK <ininidinji@gmail.com>
2026-05-13 00:28:00 +0530

    api url update

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Without this, the browser will block requests from React to FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://to-do-list-ggrlmz5fu-shrilakshmi-s-listup.vercel.app/"],  # React's default port
+    allow_origins=["*"],  # React's default port
     allow_credentials=True,
     allow_methods=["*"],   # Allow GET, POST, PUT, DELETE
     allow_headers=["*"],
